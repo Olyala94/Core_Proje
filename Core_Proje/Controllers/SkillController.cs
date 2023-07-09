976b7a491195036e97d7a14a735a9f39ab010e16
Olya <119108499+Olyala94@users.noreply.github.com>
2023-07-09 14:26:16 +0300

    # - Yeteneklerim Kısmı CRUD işlemleri Tamamlandı

--- Core_Proje/Controllers/SkillController.cs
+++ Core_Proje/Controllers/SkillController.cs
@@ -33,5 +33,30 @@ namespace Core_Proje.Controllers
             skillManager.TAdd(skill);
             return RedirectToAction("Index");
         }
+
+        public IActionResult DeleteSkill(int id)
+        {
+            var values = skillManager.TGetByID(id);   
+            skillManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditSkill(int id)
+        {
+
+            ViewBag.v1 = "Düzenleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Düzenleme";
+            var values = skillManager.TGetByID(id);
+            return View(values);   
+        }
+
+        [HttpPost]
+        public IActionResult EditSkill(Skill skill)
+        {
+            skillManager.TUpdate(skill);
+            return RedirectToAction("Index");
+        }
     }
 }
