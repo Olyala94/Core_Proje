67ab2a704e0b98f2411d79975d76911c8443c55d
Olya <119108499+Olyala94@users.noreply.github.com>
2023-07-12 22:29:59 +0300

    # - Fluent Validationlar Yapıldı , ve  anlatıldı

--- Core_Proje/Controllers/PortfolioController.cs
+++ Core_Proje/Controllers/PortfolioController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
@@ -21,24 +23,68 @@ namespace Core_Proje.Controllers
         [HttpGet]
         public IActionResult AddPortfolio()
         {
+            ViewBag.v1 = "Proje Listesi";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Proje Ekle";
             return View();
         }
 
         [HttpPost]
         public IActionResult AddPortfolio(Portfolio portfolio)
         {
-            ViewBag.v1 = "Proje Listesi";
-            ViewBag.v2 = "Projeler";
-            ViewBag.v3 = "Proje Ekle";
-            if (portfolio != null && portfolio.ImageUrl != "" && portfolio.Name.Length >= 5 && portfolio.Name.Length <= 100)
+            PortfolioValidator portfolioValidator = new PortfolioValidator();
+            ValidationResult results = portfolioValidator.Validate(portfolio);
+            if (results.IsValid)
             {
                 portfolioManager.TAdd(portfolio);
+                return RedirectToAction("Index");
             }
             else
             {
-                //hata mesajı ver 
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
             }
+            return View();
+        }
+
+        public IActionResult DeletePortfolio(int id)
+        {
+            var values = portfolioManager.TGetByID(id);
+            portfolioManager.TDelete(values);
             return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditPortfolio(int id)
+        {
+
+            ViewBag.v1 = "Proje Listesi";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Proje Düzenle";
+            var values = portfolioManager.TGetByID(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult EditPortfolio(Portfolio portfolio)
+        {
+            PortfolioValidator validations = new PortfolioValidator();
+            ValidationResult result = validations.Validate(portfolio);
+            if (result.IsValid)
+            {
+                portfolioManager.TUpdate(portfolio);
+                return RedirectToAction("Index");
+            }
+            else 
+            {
+               foreach(var item in result.Errors)
+               {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+               }
+            }   
+            return View(); 
         }
     }
 }
