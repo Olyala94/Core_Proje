ee21871985417c99e4b4cf57b1e488b46b54fa7c
Olya <119108499+Olyala94@users.noreply.github.com>
2023-08-07 16:28:29 +0300

    # - Api ile Hava durumu bilğisi çekme

--- Core_Proje/Areas/Writer/Controllers/DashboardController.cs
+++ Core_Proje/Areas/Writer/Controllers/DashboardController.cs
@@ -1,13 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace Core_Proje.Areas.Writer.Controllers
 {
     [Area("Writer")]
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashboardController(UserManager<WriterUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = values.Name + " " + values.Surname;
+
+
+            //Wheater Api
+            string api = "ba91e6d23eb71ac0096fadc684229e3c";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.api = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
+            //statistics
+            Context c = new Context();
+            @ViewBag.v1 = 0;
+            @ViewBag.v2 = c.Announcements.Count();
+            @ViewBag.v3 = 0;
+            @ViewBag.v4 = c.Skills.Count();
+
             return View();
         }
     }
 }
+
