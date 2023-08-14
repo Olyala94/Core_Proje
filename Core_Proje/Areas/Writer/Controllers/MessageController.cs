1738466be4fb8a05b61c6d34d8bc0554558f968a
olyadev <yusupowa9494@mail.ru>
2023-08-14 19:04:11 +0300

    # -  Giden Mesaj Detayları

--- Core_Proje/Areas/Writer/Controllers/MessageController.cs
+++ Core_Proje/Areas/Writer/Controllers/MessageController.cs
@@ -34,5 +34,19 @@ namespace Core_Proje.Areas.Writer.Controllers
 
             return View(messageList);
         }
+
+        
+        public IActionResult MessageDetails(int id)
+        {
+            WriterMessage writerMessage = writerMessageManager.TGetByID(id);
+            return View(writerMessage);
+        }
+
+        
+        public IActionResult ReceiverMessageDetails(int id)
+        {
+            WriterMessage writerMessage = writerMessageManager.TGetByID(id);
+            return View(writerMessage);
+        }
     }
 }
