e6356911af9dadcd6f458d108f2d9a4a2f82c2fb
olyadev <yusupowa9494@mail.ru>
2023-08-15 07:09:36 +0300

    156 - Dashboard Eksik İstatistiklerin Tamamlanması yapıldı.

--- Core_Proje/Areas/Writer/Controllers/DashboardController.cs
+++ Core_Proje/Areas/Writer/Controllers/DashboardController.cs
@@ -30,9 +30,9 @@ namespace Core_Proje.Areas.Writer.Controllers
 
             //statistics
             Context c = new Context();
-            @ViewBag.v1 = 0;
+            @ViewBag.v1 = c.WriterMessages.Where(xx=>xx.Receiver == values.Email).Count();
             @ViewBag.v2 = c.Announcements.Count();
-            @ViewBag.v3 = 0;
+            @ViewBag.v3 = c.Users.Count();
             @ViewBag.v4 = c.Skills.Count();
 
             return View();
